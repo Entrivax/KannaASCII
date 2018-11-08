d7ac88072a205e349ccbb9934153fdae4e2dcb5b
Lorenzo Pilotto <lpilotto@knowmore.fr>
2018-11-08 19:02:51 +0100

    initial commit

---
+++ KannaASCII/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace KannaASCII
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Consolas", 5.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 8);
+            this.label1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 525);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ KannaASCII/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KannaASCII
+{
+    public partial class Form1 : Form
+    {
+        int targetBleed = 20;
+        int offset = 35;
+        string template;
+        public Form1()
+        {
+            InitializeComponent();
+
+            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            this.BackColor = Color.Transparent;
+            this.TransparencyKey = Color.Transparent;
+            Location = new Point(60, 200);
+
+            template = File.ReadAllLines("./kanna.txt").Aggregate((a, b) => a + '\n' + b);
+            var templateSize = TextRenderer.MeasureText(template, new Font("Consolas", 5.25f, GraphicsUnit.Point));
+            this.Size = new Size(templateSize.Width + TextRenderer.MeasureText(" ", new Font("Consolas", 5.25f, GraphicsUnit.Point)).Width * offset * targetBleed, templateSize.Height);
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e) { /* Ignore */ }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                var lastValue = "";
+                for (var i = 0; i < targetBleed; i++)
+                {
+                    lastValue = BleedTemplate(lastValue, i * offset, template);
+
+                    this.BeginInvoke(new Action(() =>
+                    {
+                        label1.Text = lastValue;
+                    }));
+
+                    await Task.Delay(50);
+                }
+            });
+        }
+
+        private static string BleedTemplate(string original, int charsOffset, string template)
+        {
+            var originalLines = original.Split(new[] { '\n' });
+            var templateLines = template.Split(new[] { '\n' });
+            var lineCount = Math.Max(originalLines.Length, templateLines.Length);
+            var newLines = new List<string>();
+            for (var i = 0; i < lineCount; i++)
+            {
+                newLines.Add(BleedLine(i < originalLines.Length ? originalLines[i] : null, charsOffset, i < templateLines.Length ? templateLines[i] : null));
+            }
+            return newLines.Aggregate((a, b) => a + '\n' + b);
+        }
+
+        private static string BleedLine(string original, int charsOffset, string newLine)
+        {
+            var builder = new StringBuilder(Math.Max(newLine.Length + charsOffset, original?.Length ?? 0));
+            builder.Append(original?.PadRight(charsOffset, ' ') ?? "".PadRight(charsOffset, ' '), 0, charsOffset);
+            var lineLength = Math.Max(newLine.Length, (original?.Length ?? 0) - charsOffset);
+            for (var i = 0; i < lineLength; i++)
+            {
+                builder.Append(BleedChar((original?.Length ?? 0) > i + charsOffset ? original[i + charsOffset] : ' ', (newLine?.Length ?? 0) > i ? newLine[i] : ' '));
+            }
+            return builder.ToString();
+        }
+
+        private static char BleedChar(char originalChar, char newChar)
+        {
+            return newChar != ' ' ? newChar : originalChar;
+        }
+    }
+}
